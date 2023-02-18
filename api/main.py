7387fc3ac5fa65246955096a772a81600a84c6cf
root <bobwatcherx@gmail.com>
2023-02-18 07:41:06 +0700

    new

--- api/main.py
+++ api/main.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask,request
 app = Flask(__name__)
 
 @app.route("/")
 def home():
 	return "HELLO from vercel use flask"
 
+@app.route('/example', methods=['POST'])
+def example():
+    if request.method == 'POST':
+        data = request.form['data']
+        print(data)
+        return f"The data you sent is: {data}"
 
 @app.route("/about")
 def about():
